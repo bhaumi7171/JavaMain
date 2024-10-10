ff5bf604d90856ff23a9e2b5557a87b9094aca2a
binoo <binoo@BHAUMI>
2024-10-10 12:42:48 +0530

    Updated Java

---
+++ Sample/src/main/java/forloop/Fornumbers.java
@@ -0,0 +1,15 @@
+package forloop;
+
+public class Fornumbers {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a;
+for(a=1;a<=10;a++)
+{
+	System.out.println(a);
+}
+	}
+
+}
