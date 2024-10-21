9e9ad9b80326aabc3705bbf0352695ddf86b34a3
binoo <binoo@BHAUMI>
2024-10-21 12:44:24 +0530

    updated

---
+++ Sample/src/main/java/Command.java
@@ -0,0 +1,15 @@
+
+public class Command {
+	
+		public class CommandLineExample
+		{  
+		public void main(String args[])
+		{  
+		System.out.println("Your First argument is: "+args[0]);  
+		System.out.println("Your Second argument is: "+args[1]);
+		System.out.println("Your Third argument is: "+args[2]);    
+		}  
+		}  
+}
+	
+
