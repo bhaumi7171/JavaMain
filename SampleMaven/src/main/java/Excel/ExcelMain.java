f61efb60816fc221b814620ed562a63c0c6dcd4e
binoo <binoo@DESKTOP-79EU623>
2024-10-24 11:56:23 +0530

    update

---
+++ SampleMaven/src/main/java/Excel/ExcelMain.java
@@ -0,0 +1,13 @@
+package Excel;
+
+import java.io.IOException;
+
+public class ExcelMain {
+	// TODO Auto-generated method stub
+	public static void main(String args[]) throws IOException
+	{
+System.out.println(Excelcode.getStringData(1, 0));
+System.out.println(Excelcode.getIntegerData(1, 1));
+}
+}
+
